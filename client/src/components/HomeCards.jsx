c78d0cac50787439574058fb1d50e6ab8cd0c785
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-05-12 21:36:00 +0530

    updated hrefs for anchors in homecards and navbar

--- client/src/components/HomeCards.jsx
+++ client/src/components/HomeCards.jsx
@@ -10,7 +10,7 @@ const HomeCards = () => {
                 Browse our React jobs and start your career today
               </p>
               <a
-                href="/jobs.html"
+                href="/jobs"
                 className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
               >
                 Browse Jobs
@@ -22,7 +22,7 @@ const HomeCards = () => {
                 List your job to find the perfect developer for the role
               </p>
               <a
-                href="/add-job.html"
+                href="/add-job"
                 className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
               >
                 Add Job
