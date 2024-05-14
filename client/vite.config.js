152d73634edd9a23b528f4384fb3caa4ea579fdf
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-05-14 14:30:14 +0530

    updated required pages for completion

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   port: 3001,
-  // }
-})
+  server: {
+    // port: 3001,
+    proxy: {
+      "/api": {
+        target: "https://talent-trove-api.onrender.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
